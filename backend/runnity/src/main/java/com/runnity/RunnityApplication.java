e09f9c64656282f04b008a3c8ecc8382a188173f
DESKTOP-DLOPF19\SSAFY <daum0604@naver.com>
2025-10-30 16:56:26 +0900

    initial commit : backend

---
+++ backend/runnity/src/main/java/com/runnity/RunnityApplication.java
@@ -0,0 +1,13 @@
+package com.runnity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnityApplication.class, args);
+    }
+
+}
---
+++ backend/runnity/src/test/java/com/runnity/RunnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runnity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
