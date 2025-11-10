b2c921d414832744f3e1d2950afcc9b2a09e6466
dayea <myannnng@gmail.com>
2025-11-10 19:39:01 +0900

    S13P31A703-161 feat : 워치 기본 세팅 - 워치 기본 세팅

---
+++ frontend/watch/src/main/java/com/example/runnity/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.runnity.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Runnity 디자인 시스템 컬러 팔레트
+object ColorPalette {
+    object Light {
+        val primary = Color(0xFF000000)              // #000000
+        val secondary = Color(0xFF848487)            // #848487
+        val component = Color(0xFF9E9E9E)            // #9E9E9E
+        val containerBackground = Color(0xFFF4F4F4) // #F4F4F4
+        val background = Color(0xFFFFFFFF)           // #FFFFFF
+    }
+
+    object Common {
+        val accent = Color(0xFF595858)               // #595858
+        val stopAccent = Color(0xFFFF6868)           // #FF6868
+    }
+}
