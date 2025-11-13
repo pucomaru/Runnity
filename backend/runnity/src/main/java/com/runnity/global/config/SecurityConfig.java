833dc09d49db00556490d999ccb6d26542117b60
J0YERIM <chosarah70@gmail.com>
2025-11-13 15:30:12 +0900

    chore: 시큐리티 설정 변경

--- backend/runnity/src/main/java/com/runnity/global/config/SecurityConfig.java
+++ backend/runnity/src/main/java/com/runnity/global/config/SecurityConfig.java
@@ -44,12 +44,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/api/v1/challenges/**",
-                                "/api/v1/me/**",
-                                "/api/v1/stats/**",
                                 "/api/v1/auth/login/**",
-                                "/api/v1/auth/token",
-                                "/api/v1/notifications/**"
+                                "/api/v1/auth/token"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
