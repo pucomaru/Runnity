a439b5db2934d51a11a2056c11c2071cd3251c04
김동휘 <k970804@naver.com>
2025-11-19 11:16:03 +0900

    fix: 알림 문구 변경

--- backend/stream/src/main/java/com/runnity/stream/scheduler/domain/NotificationTemplate.java
+++ backend/stream/src/main/java/com/runnity/stream/scheduler/domain/NotificationTemplate.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationTemplate {
-    CHALLENGE_READY("[런니티] 챌린지 시작", "챌린지 시작 5분 전입니다. 곧 입장 가능합니다!"),
-    CHALLENGE_DONE("[런니티] 챌린지 종료", "챌린지가 종료되었습니다. 수고하셨습니다!");
+    CHALLENGE_READY("[Runnity] 챌린지 시작", "챌린지 시작 5분 전입니다. 곧 입장 가능합니다!"),
+    CHALLENGE_DONE("[Runnity] 챌린지 종료", "챌린지가 종료되었습니다. 수고하셨습니다!");
 
     private final String title;
     private final String body;
